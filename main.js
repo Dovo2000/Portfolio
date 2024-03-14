638bcfff63c5d3d5d9b4c7c5868914d6cf561d62
David Martin <dovo2000@gmail.com>
2024-03-14 09:16:30 +0100

    javascript done

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
-const cv_btn = document.getElementById("download_cv");
+const items = document.querySelectorAll(".button_list li");
 
-const userLang = navigator.language || navigator.userLanguage;
+items.forEach((item) => {
+  item.addEventListener("click", function () {
+    // Get the selected state of the clicked element
+    let selected = this.classList.contains("selected");
+    items.forEach((el) => el.classList.remove("selected"));
+    // Toggle the selected state of the clicked element
+    if (selected) {
+      this.classList.remove("selected");
+    } else {
+      this.classList.add("selected");
+    }
+  });
+});
 
-if (userLang == "es") {
-  cv_btn.href = "Assets/Documents/CV_David_Martin_ESP.pdf"; // TODO: fix this (not working properly)
-} else {
-  cv_btn.href = "Assets/Documents/CV_David_Martin_ENG.pdf";
+setCVLanguage();
+
+function setCVLanguage() {
+  const cv_btn = document.getElementById("download_cv");
+
+  const userLang = navigator.language || navigator.userLanguage;
+  if (userLang == "es") {
+    cv_btn.href = "Assets/Documents/CV_David_Martin_ESP.pdf";
+  } else {
+    cv_btn.href = "Assets/Documents/CV_David_Martin_ENG.pdf";
+  }
 }
